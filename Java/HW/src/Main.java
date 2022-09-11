43a85cc3ef2a413f210af5a3f75d546ed5f8397b
Lobach Oksana <knopa_407@ukr.net>
2022-09-11 22:18:51 +0300

    add home work Java

---
+++ Java/HW/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 12;
+        if (a==b){
+            System.out.println((a+b)*2);
+        }else
+            System.out.println(a+b);
+
+
+    }
+}
