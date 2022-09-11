7724a2537fc03e5d6d1ac1e63435860ea36a709a
Lobach Oksana <knopa_407@ukr.net>
2022-09-12 01:13:48 +0300

    add home work Java

---
+++ Java/HW/src/ex.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ex {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your number between 1 and 100: ");
+        int userNumber=scan.nextInt();
+       double remFromDivision=userNumber% 2;
+        if (remFromDivision==0){
+            System.out.println("you enter paired number");
+        }else
+            System.out.println("you enter unpaired number");
+    }
+}
---
+++ Java/HW/src/exfour.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exfour {
+    public static void main(String[] args) {
+    }
+    Scanner scan = new Scanner(System.in);
+
+
+
+
+
+}
